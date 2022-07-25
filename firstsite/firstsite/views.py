0507ffb41e8228e9ccc25e13a2b0b8ff05ce2be8
vasifismayilov <ismayilov2265@gmail.com>
2022-07-25 22:10:56 +0400

    newsite

---
+++ firstsite/firstsite/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def post(request):
+    return render(request, 'post.html')
+
+def about(request):
+    return render(request, 'about.html')
